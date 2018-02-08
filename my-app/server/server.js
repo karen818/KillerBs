63e1f4f11df15af218548d9f6b720a4f49a3d5fe
Jason <jason.jiron@gmail.com>
2018-02-07 21:00:45 -0600

    added mLab db

--- my-app/server/server.js
+++ my-app/server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 const {Job} = require('./models/Jobs.js');
-const {Employer} = require('./models/Emplyers.js');
+const {Employer} = require('./models/Employers.js');
 const {Student} = require('./models/Students.js');
 const {mongoose} = require('mongoose');
 
@@ -11,9 +12,39 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render('Things are happening');
+var db = 'mongodb://JasonJiron:milkshake8@ds225078.mlab.com:25078/killerbs';
+console.log("mLab is working");
+
+
+////////////////////////////////////////////////////////////////
+
+
+app.get('/login', (req, res) => {});
+
+app.post('/login', (req, res) => {
+  var newLogin = req.body;
+  newLogin.createDate = new Date();
+
+  if (!(req.body.firstName || req.bodylastName)) {
+    handleError(res, "Invalid user", "Must provide first and last name.", 400);
+  }
+
+  db.collection(Students).insertOne(newLogin, () => (err, doc) {
+    if (err) {
+        handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+
+    }
+  });
 });
+
+app.get('/student/login/:id', (req, res) => {});
+
+app.put('/student/login/:id', (req, res) => {});
+
+app.delete('/student/login/:id', (req, res) => {});
+
 
 
 
