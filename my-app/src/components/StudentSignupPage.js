74dd540cd46ef4553bd653730dfe7f21e96001a3
Caleb Brenner <calebwill91@gmail.com>
2018-02-16 19:32:25 -0600

    Trying to push jonathan's code for StudentSignupPage.js

--- my-app/src/components/StudentSignupPage.js
+++ my-app/src/components/StudentSignupPage.js
@@ -4,41 +4,130 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import StudentHeader from './StudentHeader';
-
 const style = {
   margin: 12,
 };
-
 class StudentSignUpPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      major: '',
+      skills: '',
+      email: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    //handles when text values in form are updated
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    console.log('You clicked the submit button!');
+    event.preventDefault();
+  }
   render() {
     return (
       <div>
-        <StudentHeader />
         <h2>Tell us a little about yourself...</h2>
-        
-        <p>First Name: <TextField hintText="First name" /></p>
-        <p>Last Name: <TextField hintText="Last name" /></p>
-        <p>Major/Field of Study: <TextField hintText="e.g. Software Development" /></p>
-        <p>Skills: 
-        	<TextField
-			      hintText="...enter job description..."
-			      multiLine={true}
-			      // rows={2}
-			      fullWidth={false}
-			    />
-    		</p>
-        <p>Email: <TextField hintText="Must use ACC email" /></p>
-        <p>Password: <TextField hintText="Password requirements go here" /></p>
-        <p>Re-type password: <TextField hintText="Re-enter password for veriication" /></p>
-
-
-        <RaisedButton label="Submit" primary={true} style={style} />
-        <RaisedButton label="Cancel" secondary={true} style={style} />
+        <form onSubmit={this.handleSubmit}>
+          <p>
+          
+            
+            <TextField 
+            //this field represents possible v2 for each field
+              name='firstName'
+              defaultValue='...type your first name here'
+              floatingLabelText='First Name'
+              hintText='...type your first name here'
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+           
+          </p>
+          <p>
+          <label>
+            Last Name: 
+            <TextField 
+              name='lastName'
+              hintText='...type your last name here'
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>
+          <p>
+          <label>
+            Major: 
+            <TextField 
+              //future version may include major selector from prepopulated list
+              name='major'
+              hintText="...type your major here" 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>    
+          </p>
+          <p>
+          <label>
+            Skills: 
+            <TextField 
+              //future version may include checkbox to select from prepopulated list
+              name='skills'
+              hintText="..." 
+              multiLine={true}
+              //rows={2}
+              fullWidth={false}
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>    
+          </p>
+          <p>
+          <label>
+            Email: 
+            <TextField 
+              name='email'
+              hintText="First Name" 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>  
+          <p>
+          <label>
+            Create Password: 
+            <TextField 
+              name='test'
+              type='password'
+              hintText="password" 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>  
+            
+          <p>
+          <label>
+            Re-type Password: 
+            <TextField 
+              name='test'
+              type='password'
+              hintText='Re-enter password for veriication' 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>
+            <RaisedButton label="Submit" primary={true} style={style} />
+            <RaisedButton label="Cancel" secondary={true} style={style} />
+        </form>
       </div>
-
     );
   }
 }
-
 export default StudentSignUpPage;
