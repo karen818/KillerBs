a4c29019ab271c427540a2fd3f3bf4eca747dd54
JonnyTFunk <jonnytfunk@gmail.com>
2018-02-16 18:47:12 -0600

    StudentSignupPage.js updated

--- my-app/src/components/StudentSignupPage.js
+++ my-app/src/components/StudentSignupPage.js
@@ -9,22 +9,138 @@ const style = {
   margin: 12,
 };
 
-class StudentSignupPage extends React.Component {
-  render () {
+class StudentSignUpPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      major: '',
+      skills: '',
+      email: '',
+      password: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    //handles when text values in form are updated
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('You clicked the submit button!');
+    event.preventDefault();
+  }
+
+  render() {
     return (
       <div>
-        <h1>StudentSignupPage.js</h1>
         <h2>Tell us a little about yourself...</h2>
-        	<p>Name: <TextField hintText="e.g. Hacker Bandits"/></p>
-        	
-        	<p>Email: <TextField hintText="Must use ACC email"/></p>
-        	<p>Password: <TextField hintText="Password requirements go here"/></p>
-        	<p>Re-type password: <TextField hintText="Re-enter password for veriication"/></p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+          
+            
+            <TextField 
+            //this field represents possible v2 for each field
+              name='firstName'
+              defaultValue='...type your first name here'
+              floatingLabelText='First Name'
+              hintText='...type your first name here'
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+           
+          </p>
 
-        <RaisedButton label="Submit" primary={true} style={style} />
+          <p>
+          <label>
+            Last Name: 
+            <TextField 
+              name='lastName'
+              hintText='...type your last name here'
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>
+
+          <p>
+          <label>
+            Major: 
+            <TextField 
+              //future version may include major selector from prepopulated list
+              name='major'
+              hintText="...type your major here" 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>    
+          </p>
+
+          <p>
+          <label>
+            Skills: 
+            <TextField 
+              //future version may include checkbox to select from prepopulated list
+              name='skills'
+              hintText="..." 
+              multiLine={true}
+              //rows={2}
+              fullWidth={false}
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>    
+          </p>
+
+          <p>
+          <label>
+            Email: 
+            <TextField 
+              name='email'
+              hintText="First Name" 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>  
+
+          <p>
+          <label>
+            Create Password: 
+            <TextField 
+              name='test'
+              type='password'
+              hintText="password" 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>  
+            
+          <p>
+          <label>
+            Re-type Password: 
+            <TextField 
+              name='test'
+              type='password'
+              hintText='Re-enter password for veriication' 
+              value={this.state.value} 
+              onChange={this.handleChange} 
+            />            
+          </label>
+          </p>
+
+            <RaisedButton label="Submit" primary={true} style={style} />
+            <RaisedButton label="Cancel" secondary={true} style={style} />
+        </form>
       </div>
+
     );
   }
 }
 
-export default StudentSignupPage;
+export default StudentSignUpPage;
