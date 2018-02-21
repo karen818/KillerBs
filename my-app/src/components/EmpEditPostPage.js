c549164ecd8a5e341dc5e7f28db2690bd44970f3
Caleb Brenner <calebwill91@gmail.com>
2018-02-21 17:08:00 -0600

    added local state logic to get form data to show up in alert box for the time being in EmpEditPostPage.js

--- my-app/src/components/EmpEditPostPage.js
+++ my-app/src/components/EmpEditPostPage.js
@@ -5,47 +5,149 @@ import EmployerHeader from './EmployerHeader';
 import { Link } from 'react-router-dom';
 
 class EmpEditPostPage extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      jobTitle: '',
+      companyName: '',
+      jobDescription: '',
+      skills: '',
+      url: '',
+      location: ''
+		};
+		
+		this.handleSubmit = this.handleSubmit.bind(this);
+
+		this.handleJobTitleUpdate = this.handleJobTitleUpdate.bind(this);
+		this.handleCompanyNameUpdate = this.handleCompanyNameUpdate.bind(this);
+		this.handleJobDescriptionUpdate = this.handleJobDescriptionUpdate.bind(this);
+		this.handleSkillsUpdate = this.handleSkillsUpdate.bind(this);
+		this.handleUrlUpdate = this.handleUrlUpdate.bind(this);
+		this.handleLocationUpdate = this.handleLocationUpdate.bind(this);
+	}
+
+	handleSubmit(event) {
+    alert('EmpEditPostPage --> '
+      + this.state.jobTitle + ' | '
+      + this.state.companyName + ' | '
+      + this.state.jobDescription + ' | '
+      + this.state.skills + ' | '
+      + this.state.url + ' | '
+      + this.state.location + '.'
+    );
+    event.preventDefault();
+	}
+	
+	handleJobTitleUpdate(event) {
+		this.setState({jobTitle: event.target.value});
+	}
+
+	handleCompanyNameUpdate(event) {
+		this.setState({companyName: event.target.value});
+	}
+
+	handleJobDescriptionUpdate(event) {
+		this.setState({jobDescription: event.target.value});
+	}
+
+	handleSkillsUpdate(event) {
+		this.setState({skills: event.target.value});
+	}
+
+	handleUrlUpdate(event) {
+		this.setState({url: event.target.value});
+	}
+
+	handleLocationUpdate(event) {
+		this.setState({location: event.target.value});
+	}
+
   render () {
     return (
       <div>
 				<EmployerHeader />
         <h2>Edit Job Posting</h2>
+
+			<form onSubmit={this.handleSubmit}>
         <p>Job Title: 
-        	<TextField 
+					<TextField
+						name="jobTitle" 
         		hintText="...edit job title..."
-        		fullWidth={true}
+						fullWidth={true}
+						onChange={this.handleJobTitleUpdate}
+            value={this.setState.jobTitle}
         	/>
         </p>
+
+				<p>Company Name: 
+        	<TextField
+						name="companyName"
+			      hintText="...enter company name..."
+			      multiLine={true}
+			      // rows={2} allows two rows to be viewed simultaneously 
+			      rows={2}
+						fullWidth={true}
+						onChange={this.handleCompanyNameUpdate}
+            value={this.setState.companyName}
+			    />
+    		</p>
         
         <p>Job Description: 
         	<TextField
+						name="jobDescription"
 			      hintText="...edit job description..."
 			      multiLine={true}
 			      // rows={2} allows two rows to be viewed simultaneously 
 			      rows={2}
-			      fullWidth={true}
+						fullWidth={true}
+						onChange={this.handleJobDescriptionUpdate}
+            value={this.setState.jobDescription}
 			    />
     		</p>
 
     		<p>Job Skills/Requirements: 
-    			<TextField 
+					<TextField 
+						name="skills"
     				hintText="...edit job requirements..."
-    				fullWidth={true}
+						fullWidth={true}
+						onChange={this.handleSkillsUpdate}
+            value={this.setState.skills}
     			/>
     		</p>
         <p>Company URL: 
-        	<TextField 
+					<TextField 
+						name="url"
         		hintText="...edit URL..."
-        		fullWidth={true}
+						fullWidth={true}
+						onChange={this.handleUrlUpdate}
+            value={this.setState.url}
         	/>
         </p>
+
+				<p>Location: 
+					<TextField 
+						name="location"
+        		hintText="...enter location..."
+						fullWidth={true}
+						onChange={this.handleLocationUpdate}
+            value={this.setState.location}
+        	/>
+        </p>
+
 				<RaisedButton 
 					containerElement={<Link to="/employer-dash" />}
 					label="Cancel" 
 					primary={true} 
 					fullWidth={true} 
 				/>
-        <RaisedButton label="Update & Save" secondary={true} fullWidth={true} />
+				<RaisedButton 
+					label="Update & Save" 
+					secondary={true} 
+					fullWidth={true} 
+					type="submit"
+				/>
+			</form>
+
       </div>
     );
   }
