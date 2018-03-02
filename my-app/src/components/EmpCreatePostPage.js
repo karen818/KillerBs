8f6928ca5e7e1cba5c07df2c615eca05d0d0131b
Caleb Brenner <calebwill91@gmail.com>
2018-03-01 19:26:35 -0600

    modified employer create post page and pushing jonathan's code to avoid the git/merge mess we hit initially when he pushed

--- my-app/src/components/EmpCreatePostPage.js
+++ my-app/src/components/EmpCreatePostPage.js
@@ -2,119 +2,121 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import EmployerHeader from './EmployerHeader';
+import colors from 'material-ui/styles/colors';
 import { Link } from 'react-router-dom';
 import { submitCreateJob } from '../actions/employerActions';
-
 class EmpCreatePostPage extends React.Component {
 	constructor(props) {
-    super(props);
-    this.state = {
-      jobTitle: '',
-      companyName: '',
-      jobDescription: '',
-      skills: '',
-      url: '',
-      location: ''
+		super(props);
+		this.state = {
+			jobTitle: '',
+			companyName: '',
+			jobDescription: '',
+			skills: '',
+			url: '',
+			location: ''
 		};
 	}
-
 	handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    submitCreateJob(this.state);
-  }
-
-  render () {
-    return (
-      <div>
+		this.setState({ [event.target.name]: event.target.value });
+	};
+	handleSubmit = (event) => {
+		event.preventDefault();
+		submitCreateJob(this.state);
+	}
+	render() {
+		return (
+			<div>
 				<EmployerHeader />
-        <h1>EmpCreatePostPage.js</h1>
-        <h2>Jorb Creator 9000</h2>
+				<h1>EmpCreatePostPage.js</h1>
+				<h2>Jorb Creator 9000</h2>
+				<form onSubmit={this.handleSubmit}>
+					<p>
+						<TextField
+							name='jobTitle'
+							type='text'
+							floatingLabelText='Job Title'
+							// floatingLabelFixed={true} allows floatingLabelText to always be displayed
+							hintText='...enter job title...'
+							fullWidth={false}
+							onChange={this.handleChange}
+							value={this.state.jobTitle}
+						/>
+					</p>
+					<p>
+						<TextField
+							name='companyName'
+							type='text'
+							floatingLabelText='Company Name'
+							hintText='...enter company name...'
+							fullWidth={false}
+							onChange={this.handleChange}
+							value={this.state.companyName}
+						/>
+					</p>
 
-			<form onSubmit={this.handleSubmit}>
-        <p>Job Title: 
-					<TextField 
-						name="jobTitle"
-        		hintText="...enter job title..."
-						fullWidth={true}
-						onChange={this.handleChange}
-            value={this.state.jobTitle}
-        	/>
-        </p>
-
-				<p>Company Name: 
-        	<TextField
-						name="companyName"
-			      hintText="...enter company name..."
-			      multiLine={true}
-			      // rows={2} allows two rows to be viewed simultaneously 
-			      rows={2}
-						fullWidth={true}
-						onChange={this.handleChange}
-            value={this.state.companyName}
-			    />
-    		</p>
-        
-        <p>Job Description: 
-        	<TextField
-						name="jobDescription"
-			      hintText="...enter job description..."
-			      multiLine={true}
-			      // rows={2} allows two rows to be viewed simultaneously 
-			      rows={2}
-						fullWidth={true}
-						onChange={this.handleChange}
-            value={this.state.jobDescription}
-			    />
-    		</p>
-
-    		<p>Job Skills/Requirements: 
-					<TextField 
-						name="skills"
-    				hintText="...enter job requirements..."
-						fullWidth={true}
-						onChange={this.handleChange}
-            value={this.state.skills}
-    			/>
-    		</p>
-        <p>Company URL: 
-					<TextField 
-						name="url"
-        		hintText="...enter URL..."
-						fullWidth={true}
-						onChange={this.handleChange}
-            value={this.state.url}
-        	/>
-        </p>
-				<p>Location: 
-					<TextField 
-						name="location"
-        		hintText="...enter location..."
-						fullWidth={true}
-						onChange={this.handleChange}
-            value={this.state.location}
-        	/>
-        </p>
-				<RaisedButton 
-					label="Submit" 
-					primary={true} 
-					fullWidth={true} 
-					type="submit"
-				/>
-				<RaisedButton 
-					containerElement={<Link to="/employer/dash" />}
-					label="Cancel" 
-					secondary={true} 
-					fullWidth={true} 
-				/>
-			</form>
-
-      </div>
-    );
-  }
+					<p>
+						<TextField
+							name='jobDescription'
+							type='text'
+							style={{ textAlign: 'left' }}
+							floatingLabelText='Job Description'
+							hintText='...enter job description'
+							multiLine={true}
+							// rows={2} allows two rows to be viewed simultaneously 
+							fullWidth={true}
+							onChange={this.handleChange}
+							value={this.state.jobDescription}
+						/>
+					</p>
+					<p>
+						<TextField
+							name='skills'
+							type='text'
+							floatingLabelText='Job Skills/Requirements'
+							hintText='...enter job requirements...'
+							fullWidth={true}
+							onChange={this.handleChange}
+							value={this.state.skills}
+						/>
+					</p>
+					<p>
+						<TextField
+							name='url'
+							type='text'
+							floatingLabelText='Company URL'
+							hintText='...enter URL...'
+							fullWidth={false}
+							onChange={this.handleChange}
+							value={this.state.url}
+						/>
+					</p>
+					<p>
+						<TextField
+							name='location'
+							type='text'
+							floatingLabelText='Location'
+							hintText='...enter location...'
+							fullWidth={false}
+							onChange={this.handleChange}
+							value={this.state.location}
+						/>
+					</p>
+					<RaisedButton
+						label="Submit"
+						primary={true}
+						fullWidth={false}
+						type="submit"
+					/>
+					<RaisedButton
+						containerElement={<Link to="/employer/dash" />}
+						label="Cancel"
+						secondary={true}
+						fullWidth={false}
+					/>
+				</form>
+			</div>
+		);
+	}
 }
-
 export default EmpCreatePostPage;
